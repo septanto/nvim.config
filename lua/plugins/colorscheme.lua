2be152b97ea94cdf87f34a1e0cea3a812ed888dc
septanto <s.s.adi.nugroho@gmail.com>
2025-12-30 10:58:48 +0700

    default color to gihub_light

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,11 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "projekt0n/github-nvim-theme" },
   { "navarasu/onedark.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_light",
+    },
+  },
 }
