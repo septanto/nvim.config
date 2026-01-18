dfeb4e32ff9e8aa64446c3d3c7769fb69fe7d4c3
septanto <s.s.adi.nugroho@gmail.com>
2026-01-18 10:13:40 +0700

    add mini move

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  -- Neovide Config-
+  -- vim.g.neovide_opacity = 0.95
+  -- vim.g.neovide_normal_opacity = 0.95
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.g.neovide_cursor_animation_length = 0.110
+  vim.g.neovide_cursor_short_animation_length = 0.03
+  vim.g.neovide_cursor_trail_size = 0.8
+end
