ebe3777ef80c5f8da5d30be5d0db1c058cc7df96
septanto <s.s.adi.nugroho@gmail.com>
2026-02-02 22:31:33 +0700

    add better escape

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,16 +2,15 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "projekt0n/github-nvim-theme" },
   { "navarasu/onedark.nvim" },
-
+  {"rebelot/kanagawa.nvim"},
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = function()
         vim.schedule(function()
           if vim.o.background == "light" then
-            vim.cmd("colorscheme github_light")
-          else
-            vim.cmd("colorscheme tokyonight")
+            vim.cmd("colorscheme onedark")
+          else vim.cmd("colorscheme tokyonight")
           end
         end)
       end,
