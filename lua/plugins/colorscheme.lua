12b60b849f8af7307f42d2b9fb5af1d2057f8680
septanto <s.s.adi.nugroho@gmail.com>
2026-03-07 22:44:10 +0700

    update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "projekt0n/github-nvim-theme" },
   { "navarasu/onedark.nvim" },
-  {"rebelot/kanagawa.nvim"},
+  { "rebelot/kanagawa.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
